7c65f656ea647e60e3842de92d9b76c62ddd58a3
Bandura Andrii <bbanduraa18@gmail.com>
2022-03-19 16:19:45 +0200

    refactor OnInit to additional method that call api with button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,29 @@
 import { Component } from '@angular/core';
-import { OnInit } from "@angular/core";
 import { DataService } from "./services/data.service";
+import { LoaderService } from "./services/loader.service";
 import { ServiceInterface } from "./types/service.interface";
+import { Subject } from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  public title='magnise-test';
+export class AppComponent {
   private services?: ServiceInterface[];
+  public title='magnise-test';
   public groups?: number[];
+  public isLoaded: boolean = false;
+  public isLoading: Subject<boolean> = this.loaderService.isLoading;
 
-  constructor(private dataService: DataService) { }
+  constructor(
+    private dataService: DataService,
+    private loaderService: LoaderService
+  ) { }
 
-  ngOnInit(): void {
+  callApi(): void {
     this.dataService.getData().subscribe((services) => {
+      this.isLoaded = true;
       this.services = services;
       this.groups = this.dataService.getUniqueId(services);
     });
