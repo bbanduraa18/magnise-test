1fb65e885828b63cc41ecd27d02ce142081b862f
Bandura Andrii <bbanduraa18@gmail.com>
2022-03-19 16:19:11 +0200

    complete loader interceptor to handle the fetching data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,26 +7,40 @@ import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzSpinModule } from "ng-zorro-antd/spin";
+import { NzButtonModule } from 'ng-zorro-antd/button';
 import { ServiceComponent } from './components/service/service.component';
+import { LoaderComponent } from './components/loader/loader.component';
+import { DataService } from './services/data.service';
+import { LoaderService } from './services/loader.service';
+import { LoaderInterceptor } from "./loader.interceptor";
 
 registerLocaleData(en);
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServiceComponent
+    ServiceComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    NzTableModule
+    NzTableModule,
+    NzSpinModule,
+    NzButtonModule
   ],
-  providers: [{ provide: NZ_I18N, useValue: en_US }],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US },
+    DataService,
+    LoaderService,
+    { provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
